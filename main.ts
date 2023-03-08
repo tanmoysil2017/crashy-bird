29ca8d05ff0480c5f2834d7a5ef1ae81bdaabcae
tanmoysil2017 <tanmoysil@gmail.com>
2023-03-08 20:32:34 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let bird = game.createSprite(0, 2)
+bird.set(LedSpriteProperty.Blink, 300)
+basic.forever(function () {
+	
+})
