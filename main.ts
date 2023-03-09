9853380918896e6760dbd8791a1c17fe0ebe1341
tanmoysil2017 <tanmoysil@gmail.com>
2023-03-09 19:51:13 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-	
+    bird.change(LedSpriteProperty.Y, -1)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    bird.change(LedSpriteProperty.Y, 1)
 })
-let bird = game.createSprite(0, 2)
+let bird: game.LedSprite = null
+bird = game.createSprite(0, 2)
 bird.set(LedSpriteProperty.Blink, 300)
-basic.forever(function () {
-	
-})
+let obstacles: game.LedSprite[] = []
+let empty0bstacleY = randint(0, 4)
+for (let index = 0; index <= 4; index++) {
+    if (index != empty0bstacleY) {
+        obstacles.push(game.createSprite(4, index))
+    }
+}
